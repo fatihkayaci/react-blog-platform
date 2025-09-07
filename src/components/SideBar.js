3b480d1e5830a9f47bc7548d1326e5f210e1a0cf
Fatih Kayacı <fatihkayaci@yahoo.com>
2025-09-07 17:58:00 +0300

    finished sidebar.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,28 @@
 function SideBar() {
+  const categoryCount = 5;
   return (
     <div className="sidebar-section">
-      <p>SideBar</p>
+      <h3>📁 Categories</h3>
+      
+      <ul className="categories-list">
+        {Array.from({ length: categoryCount }, (_, index) => (
+          <div key={index}>
+            <Category categoryName={`Category ${index + 1}`} />
+            {index < categoryCount - 1 && <hr />} {/* Son elemandan sonra hr olmasın */}
+          </div>
+        ))}
+      </ul>
     </div>
+  );
+}
+function Category({categoryName}) {
+  return (
+      <li className="category-item">
+        <a href="#">
+          <span className="category-name">{categoryName}</span>
+          <span className="category-count">12</span>
+        </a>
+      </li>
   );
 }
 export default SideBar;
